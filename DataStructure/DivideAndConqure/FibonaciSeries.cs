36eb654c929822a487049c1c267a348e4bb29982
Mayank Sahay <hellomayanksahay@Gmail.com>
2019-09-02 10:43:55 +0530

    Fibinaco Series Divide and Conqure

---
+++ DataStructure/DivideAndConqure/FibonaciSeries.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructure.DivideAndConqure
+{
+    public static class FibonaciSeries
+    {
+        public static int fibonaci( int n)
+        {
+            if (n == 1)
+                return 0;
+            if(n ==2)
+                return 1;
+            else
+                return fibonaci(n - 1) + fibonaci(n - 2);
+        }
+    }
+}
--- DataStructure/Program.cs
+++ DataStructure/Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 
+using DataStructure.DivideAndConqure;
+
 namespace DataStructure
 {
     class Program
     {
         static void Main(string[] args)
         {
+
+            // Fibonaci Series Divide and Conqure
+
+            int i = FibonaciSeries.fibonaci(5);
+            Console.WriteLine(i);
+
             Console.WriteLine("Hello World!");
+
+            Console.ReadLine();
         }
     }
 }
